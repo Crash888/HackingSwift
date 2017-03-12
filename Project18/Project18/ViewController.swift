201bbc197f3294e883400d7cc393c599f199e5ac
dd <ddresearch@shaw.ca>
2017-03-11 22:28:41 -0800

    project 18

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by D D on 2017-03-11.
+//  Copyright © 2017 D D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
