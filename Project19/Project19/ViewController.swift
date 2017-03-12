0a3cd6ba85380c38dfc10d168a0fbf09912ad61b
dd <ddresearch@shaw.ca>
2017-03-11 22:40:45 -0800

    project 19

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by D D on 2017-03-11.
+//  Copyright © 2017 D D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
