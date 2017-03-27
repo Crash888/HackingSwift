bb4633695963c510ca1c8ccdc458ae6211d04050
dd <ddresearch@shaw.ca>
2017-03-26 22:02:05 -0700

    project 22

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by D D on 2017-03-26.
+//  Copyright © 2017 D D. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var distanceReading: UILabel!
+    
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        view.backgroundColor = UIColor.gray
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //  Call this function once user has decided to accept (or reject) our auth request
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
+}
+
