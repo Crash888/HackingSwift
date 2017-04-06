e6ccc6b065ecb9187fc4aeb0b9387226c70b9963
dd <ddresearch@shaw.ca>
2017-04-06 16:05:04 -0700

    project 33 update

---
+++ Project33/Project33/SubmitViewController.swift
@@ -0,0 +1,111 @@
+//
+//  SubmitViewController.swift
+//  Project33
+//
+//  Created by D D on 2017-04-06.
+//  Copyright © 2017 D D. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class SubmitViewController: UIViewController {
+
+    var genre: String!
+    var comments: String!
+    
+    var stackView: UIStackView!
+    var status: UILabel!
+    var spinner: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "You are all set!"
+        navigationItem.hidesBackButton = true
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        doSubmission()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = UIColor.gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackViewDistribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting..."
+        status.textColor = UIColor.white
+        status.font = UIFont.preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+        
+        spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        spinner .translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(spinner)
+    }
+    
+    //  Upload this stuff to iCloud
+    func doSubmission() {
+        
+        // Prep the data to send to iCloud
+        //  CKRecord works like a dictionary where we can store names and values
+        let whistleRecord = CKRecord(recordType: "Whistles")
+        whistleRecord["genre"] = genre as CKRecordValue
+        whistleRecord["comments"] = comments as CKRecordValue
+        
+        let audioURL = RecordWhistleViewController.getWhistleURL()
+        let whistleAsset = CKAsset(fileURL: audioURL)
+        whistleRecord["audio"] = whistleAsset
+        
+        //  Now time for submission
+        CKContainer.default().publicCloudDatabase.save(whistleRecord) { [unowned self] (record, error) in
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.status.text = "Error: \(error.localizedDescription)"
+                    self.spinner.stopAnimating()
+                } else {
+                    self.view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+                    self.status.text = "Done!"
+                    self.spinner.stopAnimating()
+                    
+                    ViewController.isDirty = true
+                }
+                
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneTapped))
+            }
+        }
+    }
+    
+    func doneTapped() {
+        //  This returns an array of view controllers that were popped.  We do not care
+        //  so we replace with an '_' so no unused warning is generated
+        _ = navigationController?.popToRootViewController(animated: true)
+    }
+}
