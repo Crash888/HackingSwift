f1e612b6d0e3c22c0d295645903a427025192cbd
dd <ddresearch@shaw.ca>
2017-04-10 14:28:19 -0700

    project 36 update

--- Project36/Project36/GameScene.swift
+++ Project36/Project36/GameScene.swift
@@ -9,7 +9,13 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+enum GameState {
+    case showingLogo
+    case playing
+    case dead
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var player: SKSpriteNode!
     
@@ -21,18 +27,124 @@ class GameScene: SKScene {
         }
     }
     
+    var backgroundMusic: SKAudioNode!
+    
+    //  Opening screen
+    var logo: SKSpriteNode!
+    //  GameOver Screen
+    var gameOver: SKSpriteNode!
+    
+    //  Game starts at the logo screen
+    var gameState = GameState.showingLogo
+    
     override func didMove(to view: SKView) {
         createPlayer()
         createSky()
         createBackground()
         createGround()
-        startRocks()
         createScore()
+        createLogos()
+        
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -5.0)
+        physicsWorld.contactDelegate = self
+        
+        //  The background music
+        if let musicURL = Bundle.main.url(forResource: "music", withExtension: "m4a") {
+            backgroundMusic = SKAudioNode(url: musicURL)
+            addChild(backgroundMusic)
+        }
+        
     }
-    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     
+        switch gameState {
+        case .showingLogo:
+            gameState = .playing
+            
+            let fadeOut = SKAction.fadeOut(withDuration: 0.5)
+            let remove = SKAction.removeFromParent()
+            let wait = SKAction.wait(forDuration: 0.5)
+            let activatePlayer = SKAction.run { [unowned self] in
+                //  player now responds to physics
+                self.player.physicsBody?.isDynamic = true
+                self.startRocks()
+            }
+            
+            let sequence = SKAction.sequence([fadeOut, wait, activatePlayer, remove])
+            logo.run(sequence)
+        
+        case .playing:
+            //  When the user taps we give the plane a little push upwards each time
+            //  First line removes any existing push and the second line applies a
+            //  new push.
+            player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 20))
+            
+        case .dead:
+            //  When user touches the sceen at game over we will start a new game.
+            //  This creates a fresh game scene with simple animation
+            let scene = GameScene(fileNamed: "GameScene")!
+            let transition = SKTransition.moveIn(with: SKTransitionDirection.right, duration: 1)
+            self.view?.presentScene(scene, transition: transition)
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        //  Make sure we have a player before proceeding
+        guard player != nil else { return }
+        
+        //  Causes the plane to tilt up or down based on velocity
+        let value = player.physicsBody!.velocity.dy * 0.001
+        let rotate = SKAction.rotate(toAngle: value, duration: 0.1)
+        
+        player.run(rotate)
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        //  Did the plane hit the scoring rectangle?  If so, then score a point
+        if contact.bodyA.node?.name == "scoreDetect" || contact.bodyB.node?.name == "scoreDetect" {
+            if contact.bodyA.node == player {
+                contact.bodyB.node?.removeFromParent()
+            } else {
+                contact.bodyA.node?.removeFromParent()
+            }
+            
+            let sound = SKAction.playSoundFileNamed("coin.wav", waitForCompletion: false)
+            run(sound)
+            
+            score += 1
+            
+            return
+        }
+        
+        //  Because we are potentially removingFromParent() above we need to test that we have
+        //  both objects before continuing
+        guard contact.bodyA.node != nil && contact.bodyB.node != nil else { return }
+        
+        //  Detect plane contacting anythng else.  All collisions result in a crash
+        if contact.bodyA.node == player || contact.bodyB.node == player {
+            
+            //  Blow up the plane
+            if let explosion = SKEmitterNode(fileNamed: "PlayerExplosion") {
+                explosion.position = player.position
+                addChild(explosion)
+            }
+            
+            //  Play explosion sound
+            let sound = SKAction.playSoundFileNamed("explosion.wav", waitForCompletion: false)
+            run(sound)
+            
+            gameOver.alpha = 1
+            gameState = .dead
+            backgroundMusic.run(SKAction.stop())
+            
+            //  Remove plane and stop everything from moving
+            player.removeFromParent()
+            speed = 0
+        }
     }
     
     func createPlayer() {
@@ -42,6 +154,18 @@ class GameScene: SKScene {
         player.position = CGPoint(x: frame.width / 6, y: frame.height * 0.75)
         
         addChild(player)
+        
+        player.physicsBody = SKPhysicsBody(texture: playerTexture, size: playerTexture.size())
+        //  This says report if a player 'touches' anything
+        player.physicsBody!.contactTestBitMask = player.physicsBody!.collisionBitMask
+        
+        //  Default that the player does not respond to physics.  We are on the logo screen
+        //  so just have the plane flying straight
+        player.physicsBody?.isDynamic = false
+        
+        //  This says that we should not detect 'collisions' between objects
+        //  (ie. nothing should bounce off anything else)
+        player.physicsBody?.collisionBitMask = 0
         
         let frame2 = SKTexture(imageNamed: "player-2")
         let frame3 = SKTexture(imageNamed: "player-3")
@@ -100,6 +224,11 @@ class GameScene: SKScene {
             ground.zPosition = -10
             ground.position = CGPoint(x: (groundTexture.size().width / 2.0 + (groundTexture.size().width * CGFloat(i))) , y: groundTexture.size().height / 2)
             
+            //  Pixel perfect collision with sprites
+            ground.physicsBody = SKPhysicsBody(texture: ground.texture!, size: ground.texture!.size())
+            //  The ground can get hit but it will not move
+            ground.physicsBody?.isDynamic = false
+            
             addChild(ground)
             
             //  duration is speed of the ground movement
@@ -133,17 +262,28 @@ class GameScene: SKScene {
         //  Create a top rock and a bottom rock
         //  Top rock if a flipped over version of the rock image
         let topRock = SKSpriteNode(texture: rockTexture)
+        
+        topRock.physicsBody = SKPhysicsBody(texture: rockTexture, size: rockTexture.size())
+        topRock.physicsBody?.isDynamic = false
+        
         topRock.zRotation = CGFloat.pi
         topRock.xScale = -1.0
         
         let bottomRock = SKSpriteNode(texture: rockTexture)
+        
+        bottomRock.physicsBody = SKPhysicsBody(texture: rockTexture, size: rockTexture.size())
+        bottomRock.physicsBody?.isDynamic = false
         
         topRock.zPosition = -20
         bottomRock.zPosition = -20
         
         //  Create a sprite after the rocks to detect a player passing the rocks
         //  successfully
-        let rockCollision = SKSpriteNode(color: UIColor.red, size: CGSize(width: 32, height: frame.height))
+        let rockCollision = SKSpriteNode(color: UIColor.clear, size: CGSize(width: 32, height: frame.height))
+        
+        rockCollision.physicsBody = SKPhysicsBody(rectangleOf: rockCollision.size)
+        rockCollision.physicsBody?.isDynamic = false
+        
         rockCollision.name = "scoreDetect"
         
         addChild(topRock)
@@ -187,4 +327,14 @@ class GameScene: SKScene {
         addChild(scoreLabel)
     }
     
+    func createLogos() {
+        logo = SKSpriteNode(imageNamed: "logo")
+        logo.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(logo)
+        
+        gameOver = SKSpriteNode(imageNamed: "gameover")
+        gameOver.position = CGPoint(x: frame.midX, y: frame.midY)
+        gameOver.alpha = 0
+        addChild(gameOver)
+    }
 }
